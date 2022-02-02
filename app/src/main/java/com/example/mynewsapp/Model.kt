ba580207d53280397a33dfaf7eb1cf5f9f86a96f
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-02-02 22:13:24 +0100

    model class

---
+++ app/src/main/java/com/example/mynewsapp/Model.kt
@@ -0,0 +1,5 @@
+package com.example.mynewsapp
+
+data class Model(val author: String, val title: String, val description: String, val url: String,
+                 val urlToImage: String, val publishedAt: String) {
+}
