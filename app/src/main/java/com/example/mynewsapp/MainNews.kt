a98cc8a635ab5b4db4e84efbfcd950b3b79f8e0d
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-02-02 22:12:56 +0100

    main news

---
+++ app/src/main/java/com/example/mynewsapp/MainNews.kt
@@ -0,0 +1,12 @@
+package com.example.mynewsapp
+
+class MainNews {
+
+    private val status: String = ""
+    private val totalResult: String= ""
+    val articles : ArrayList<Model>
+        get() {
+            TODO()
+        }
+
+}
