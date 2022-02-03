d3873d631fd877c6967c7fbefaf9369e06013d69
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-02-03 23:43:43 +0100

    interface implementation

--- app/src/main/java/com/example/mynewsapp/utils/ApiInterface.kt
+++ app/src/main/java/com/example/mynewsapp/utils/ApiInterface.kt
@@ -1,14 +1,19 @@
 package com.example.mynewsapp.utils
 
+import com.example.mynewsapp.MainNews
+import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiInterface {
 
-    var base_url: String = "https://newsapi.org/v2/"
+    companion object {
+        var base_url: String = "https://newsapi.org/v2/"
+    }
 
     @GET("top-headlines")
-    Call<MainNews> getNews(
-
-    )
+    fun getMainNews(@Query("country") country: String,
+        @Query("apiKey") apiKey: String, @Query("pageSize") page: Int): Call<List<MainNews >>
 
 }
