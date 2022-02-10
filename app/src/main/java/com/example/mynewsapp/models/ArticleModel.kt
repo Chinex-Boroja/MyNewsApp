b4c145a7c87493696b3abba61b35dd6513a5e2aa
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-02-10 09:34:51 +0100

    model article class

--- app/src/main/java/com/example/mynewsapp/models/ArticleModel.kt
+++ app/src/main/java/com/example/mynewsapp/models/ArticleModel.kt
@@ -1,5 +1,25 @@
 package com.example.mynewsapp.models
 
-data class  Model(val author: String, val title: String, val description: String, val url: String,
-                 val urlToImage: String, val publishedAt: String) {
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+data class ArticleModel(
+    @PrimaryKey(autoGenerate = true)
+    var id : Int? = null,
+    @SerializedName("author")
+    var author: String?,
+    @SerializedName("content")
+    var content: String?,
+    @SerializedName("description")
+    var description: String?,
+    @SerializedName("publishedAt")
+    var publishedAt: String,
+    @SerializedName("source")
+    val source: SourceStream?,
+    @SerializedName("title")
+    var title: String?,
+    @SerializedName("urlToImage")
+    val urlToImage: String?,
+) : Serializable {
 }
