7b8db535cfc236d05160b3b36a52715bb6ae40f6
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-02-11 15:43:27 +0100

    modfied constant.kt

---
+++ app/src/main/java/com/example/mynewsapp/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.mynewsapp.utils
+
+class Constants {
+    companion object {
+        const val API_KEY = "3148639a4eda4c1395d1c73eb7f19b00"
+        const val BASE_URL = "https://newsapi.org/v2/"
+        const val SEARCH_TIME_DELAY = 300L
+    }
+}
