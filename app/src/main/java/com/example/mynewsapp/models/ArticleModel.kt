acd20b4c3e7dd392be72040590e18fafd4c14f35
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-02-11 15:40:59 +0100

    modified article model

--- app/src/main/java/com/example/mynewsapp/models/ArticleModel.kt
+++ app/src/main/java/com/example/mynewsapp/models/ArticleModel.kt
@@ -1,9 +1,13 @@
 package com.example.mynewsapp.models
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
+@Entity(
+    tableName = "articles"
+)
 data class ArticleModel(
     @PrimaryKey(autoGenerate = true)
     var id : Int? = null,
@@ -14,7 +18,7 @@ data class ArticleModel(
     @SerializedName("description")
     var description: String?,
     @SerializedName("publishedAt")
-    var publishedAt: String,
+    var publishedAt: String?,
     @SerializedName("source")
     val source: SourceStream?,
     @SerializedName("title")
