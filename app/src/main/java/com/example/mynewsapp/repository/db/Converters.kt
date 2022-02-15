d5acd791f566d09b3b070420a3e9ffb7ec0c8404
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-02-15 22:08:05 +0100

    used proper converter

--- app/src/main/java/com/example/mynewsapp/repository/db/Converters.kt
+++ app/src/main/java/com/example/mynewsapp/repository/db/Converters.kt
@@ -1,16 +1,28 @@
 package com.example.mynewsapp.repository.db
 
+import androidx.room.TypeConverter
 import androidx.room.TypeConverters
 import com.example.mynewsapp.models.SourceStream
+import org.json.JSONObject
 
 class Converters {
-    @TypeConverters
-    fun fromSource(sourceStream: SourceStream) : String? {
-        return sourceStream.name
+    @TypeConverter
+    fun fromSource(source: SourceStream): String {
+        return JSONObject().apply {
+            put("id", source.id)
+            put("name", source.name)
+        }.toString()
     }
+//    fun fromSource(sourceStream: SourceStream) :String? {
+//        return sourceStream.name
+//    }
 
-    @TypeConverters
-    fun toSource(name: String) : SourceStream {
-        return SourceStream(name, name)
+    @TypeConverter
+    fun toSource(source: String): SourceStream {
+        val json = JSONObject(source)
+        return SourceStream(json.getString("id"), json.getString("name"))
     }
+//    fun toSource(name: String) : SourceStream {
+//        return SourceStream(name, name)
+//    }
 }
