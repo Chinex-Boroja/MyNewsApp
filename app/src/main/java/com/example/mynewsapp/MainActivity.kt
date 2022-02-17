9c00af0973672c2c74fb8173d02b188de13b980f
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-02-18 00:48:41 +0100

    added nav controller

--- app/src/main/java/com/example/mynewsapp/MainActivity.kt
+++ app/src/main/java/com/example/mynewsapp/MainActivity.kt
@@ -1,67 +1,38 @@
 package com.example.mynewsapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
-import androidx.viewpager.widget.ViewPager
-import com.google.android.material.tabs.TabItem
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
-import com.google.android.material.tabs.TabLayout.TabLayoutOnPageChangeListener
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.mynewsapp.repository.NewsRepository
+import com.example.mynewsapp.repository.db.ArticleModelDatabase
+import com.example.mynewsapp.viewmodels.NewsViewModel
+import com.example.mynewsapp.viewmodels.NewsViewModelFactory
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    var mHome: TabItem? = null
-    var mSports: TabItem? = null
-    var mEnt: TabItem? = null
-    var mScience: TabItem? = null
-    var mTech: TabItem? = null
-    var mHealth: TabItem? = null
-    var mToolbar: Toolbar? = null
-    lateinit var pagerAdapter: PagerAdapter
-    lateinit var mTableLayout: TabLayout
-    lateinit var viewPager: ViewPager
-
-    //Api key
-    var apiKey = "3148639a4eda4c1395d1c73eb7f19b00"
+    lateinit var viewModel: NewsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mToolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(mToolbar)
+        val newsRepository = NewsRepository(ArticleModelDatabase(this))
+        val viewModelProvider = NewsViewModelFactory(newsRepository)
+        viewModel = ViewModelProvider(this, viewModelProvider).get(NewsViewModel::class.java)
 
-        mHome = findViewById(R.id.home)
-        mSports = findViewById(R.id.sports)
-        mEnt = findViewById(R.id.ent)
-        mScience = findViewById(R.id.science)
-        mTech = findViewById(R.id.tech)
-        mHealth = findViewById(R.id.health)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.news_host) as? NavHostFragment
+        val navController = navHostFragment?.navController
 
-        viewPager = findViewById<ViewPager>(R.id.fragment_container)
-        mTableLayout = findViewById(R.id.include)
+        if (navController != null) {
+            bottomNavigationView.setupWithNavController(navController)
+        }
 
-        pagerAdapter = PagerAdapter(supportFragmentManager, 6)
-        viewPager.adapter = pagerAdapter
-
-        //if the user directly click on the tab item
-        mTableLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab) {
-                viewPager.currentItem = tab.position
-
-                if (tab.position == 0 || tab.position == 1 || tab.position == 2 ||
-                        tab.position == 3 || tab.position == 4 || tab.position == 5)
-                {
-                    pagerAdapter.notifyDataSetChanged()
-                }
-            }
-            override fun onTabUnselected(tab: TabLayout.Tab) {}
-            override fun onTabReselected(tab: TabLayout.Tab) {}
-        })
-
-        //For swipes
-        viewPager.addOnPageChangeListener(TabLayoutOnPageChangeListener(mTableLayout))
     }
 }
