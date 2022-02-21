683ac7fa7d4cf0211e45aa343bc1a80308514bfd
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-02-21 22:22:46 +0100

    Article frag

--- app/src/main/java/com/example/mynewsapp/fragments/ArticleFragment.kt
+++ app/src/main/java/com/example/mynewsapp/fragments/ArticleFragment.kt
@@ -5,15 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.navigation.fragment.navArgs
+import com.example.mynewsapp.MainActivity
 import com.example.mynewsapp.R
+import com.example.mynewsapp.viewmodels.NewsViewModel
+import kotlinx.android.synthetic.main.fragment_article.*
 
-class ArticleFragment : Fragment() {
+class ArticleFragment : Fragment(R.layout.fragment_article) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_article, container, false)
+    lateinit var viewModel: NewsViewModel
+    val args : ArticleFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as MainActivity).viewModel
+        val article = args.article
+        webView.apply {
+            webViewClient = object : WebViewClient() {
+                override fun onPageCommitVisible(view: WebView?, url: String?) {
+                    super.onPageCommitVisible(view, url)
+                    progressBar.visibility = View.GONE
+                }
+            }
+            loadUrl(article.url.toString())
+        }
     }
 }
